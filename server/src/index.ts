905370bd78375ad1350135d45441a6256a9caceb
Peter-TMK <tundepeter01@gmail.com>
2023-12-23 18:04:01 +0100

    implement fetch Product from server updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
+import cors from "cors";
 import express, { Request, Response } from "express";
 import { sampleProducts } from "./data";
 
 const app = express();
 const port = 4444;
+
+app.use(cors({ credentials: true, origin: ["http://localhost:5173"] }));
 
 app.get("/api/products", (req: Request, res: Response) => {
   res.json(sampleProducts);
